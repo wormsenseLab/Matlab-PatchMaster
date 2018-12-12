1e0564515068ab256646ebcd6377a0abaa9b9604
curiousgeorgie <biocuriousgeorgie@gmail.com>
2018-12-12 10:29:05 -0800

    plots for resonance freq

--- Analysis/SK/Scripts-in-progress/SinePowerSpec.m
+++ Analysis/SK/Scripts-in-progress/SinePowerSpec.m
@@ -75,6 +75,8 @@ sinePeaks = FrequencyAnalysis(ephysData, ephysMetaData, protList, 'matchType', m
 sinePeaksPD = FrequencyAnalysis(ephysData, ephysMetaData, protList, 'matchType', matchType, 'norm', 0, 'channel',3);
 sinePeaksStim = FrequencyAnalysis(ephysData, ephysMetaData, protList, 'matchType', matchType, 'norm', 0, 'channel',2);
 
+% sine_allExt_ant_PDfiltered(180923).xls for all
+
 % okay we're just going to plot the power spectrum in the morning and
 % ignore the bode plot because I don't know what a "system" object really
 % is in matlab or if my data can be one, or what a linear time-invariant
@@ -87,7 +89,7 @@ allPSD = [];
 allF = [];
 allFreq = [];
 
-whichPeaks = sinePeaksPD; %also change meansByFreqI at bottom
+whichPeaks = sinePeaks; %also change meansByFreqI at bottom
 % ally = []; %for plotting fft magnitude and phase
 % allf_fft = [];
 
@@ -149,33 +151,74 @@ chH = get(gca,'children');
 set(gca,'children',flipud(chH));
 plotfixer;
 
-meanPSDByFreqPD = meanPSDByFreq;
-% meanPSDByFreqI = meanPSDByFreq;
+% meanPSDByFreqPD = meanPSDByFreq;
+meanPSDByFreqI = meanPSDByFreq;
 
-%% Plot stacked power spectra
-figure(); clear axh;
+%% Read in simulated power spectra and steady-state/rms
+sim_path = 'C:\Users\Sammy\Dropbox\Goodman Lab\Posters Papers Proposals\2018-Katta-SpatiotemporalDynamics\source-data\temporal-frequency\figure_4c_stimulus.txt';
+sim_stim = dlmread(sim_path,'\t',1,0);
+sim_f = sim_stim(:,1);
+sim_stim = sim_stim(:,2:end);
+
+sim_path = 'C:\Users\Sammy\Dropbox\Goodman Lab\Posters Papers Proposals\2018-Katta-SpatiotemporalDynamics\source-data\temporal-frequency\figure_4c_response.txt';
+sim_resp = dlmread(sim_path,'\t',1,1);
+
+sim_path = 'C:\Users\Sammy\Dropbox\Goodman Lab\Posters Papers Proposals\2018-Katta-SpatiotemporalDynamics\source-data\temporal-frequency\figure_4d.txt';
+sim_steady = dlmread(sim_path,'\t',1,0);
+sim_sum_f = sim_steady(:,1);
+sim_steady = sim_steady(:,2:end);
+
+sim_path = 'C:\Users\Sammy\Dropbox\Goodman Lab\Posters Papers Proposals\2018-Katta-SpatiotemporalDynamics\source-data\temporal-frequency\figure_4e.txt';
+sim_rms = dlmread(sim_path,'\t',1,1);
+
+%% Plot stacked power spectra for sim and experimental
+
+scrsz = get(groot,'ScreenSize');
+
+% Plot experimental after removing 1kHz 
+meanPSDByFreqPD = meanPSDByFreqPD(:,~ismember(eachFreq,1000));
+meanPSDByFreqI = meanPSDByFreqI(:,~ismember(eachFreq,1000));
+eachFreq = eachFreq(~ismember(eachFreq,1000));
+
+figure('Position',[1 1 800 700]); clear axh;
 yyh = cell(0);
 
 for i = 1:size(meanPSDByFreqI,2)
     axh(i) = subtightplot(size(meanPSDByFreqI,2),1,i,0.02,0.05,0.1);
-    yyh{i} = plotyy(meanF,meanPSDByFreqI(:,i),meanF,meanPSDByFreqPD(:,i));
-    set(yyh{i}, 'YScale', 'log', 'XScale', 'log','box','off');
+    yyh{i} = plotyy(meanF,meanPSDByFreqI(:,i),meanF,meanPSDByFreqPD(:,i),@loglog);
     set(yyh{i}(1),'YLim',[1e-30 1e-20])
     set(yyh{i}(2),'YLim',[1e-10 1e0])
+    set(yyh{i}, 'YScale', 'log', 'XScale', 'log','box','off');
 end
 
 for i = 1:size(meanPSDByFreqI,2)-1
     set(yyh{i},'XTickLabel',[]);
 end
-    
 
+% Plot simulated data
+figure('Position',[1 1 800 700]); clear axh;
+yyh = cell(0);
+
+for i = 1:size(sim_resp,2)
+    axh(i) = subtightplot(size(meanPSDByFreqI,2),1,i,0.02,0.05,0.1);
+    yyh{i} = plotyy(sim_f,sim_resp(:,i),sim_f,sim_stim(:,i),@semilogx);
+    set(yyh{i},'XLim',[1 1e5],'box','off');
+    set(yyh{i}(1),'YLim',[-150 50])
+    set(yyh{i}(2),'YLim',[-150 50])
+end
+
+for i = 1:size(sim_resp,2)-1
+    set(yyh{i},'XTickLabel',[]);
+end
+    
+plotfixer
 %% Calculate steady state mean and rms by frequency
 
-whichPeaks = sinePeaksPD;
-normFlag = 1; % 1 to normalize steady-state and RMS to value for square pulse
+whichPeaks = sinePeaks;
+normFlag = 0; % 1 to normalize steady-state and RMS to value for square pulse
 extFilter = [2.5 5];
 
-eachFreq = [0 10 30 100 200 500 1000];
+eachFreq = [0 10 30 100 200 500];
 sf = 10000; %Hz
 allSteady = [];
 allRMS = [];
@@ -237,5 +280,74 @@ for iProfile = 1:length(eachFreq)
 end
 
 
-%% Export everything for Igor plotting and source data
+%% Plot experimental and simulated steady-state and RMS
 
+
+figure(); clear axh;
+yyh = cell(0);
+
+yyh = plotyy(eachFreq,meanSteadyByFreq(:,1),sim_sum_f,sim_steady,@semilogx);
+set(yyh(1),'YLim',[0 60],'YTick',(0:20:60));
+hold(yyh(1),'on');
+errorbar(yyh(1),eachFreq,meanSteadyByFreq(:,1),meanSteadyByFreq(:,3),'bo');
+set(yyh,'box','off');
+xlabel('Frequency (Hz)')
+ylabel(yyh(1),'Steady-state current (pA)');
+ylabel(yyh(2),'Normalized steady-state current');
+
+figure(); clear axh;
+yyh = cell(0);
+
+yyh = plotyy(eachFreq,meanRMSByFreq(:,1),sim_sum_f,sim_rms,@semilogx);
+set(yyh(1),'YLim',[0 8],'YTick',(0:2:8));
+hold(yyh(1),'on');
+errorbar(yyh(1),eachFreq,meanRMSByFreq(:,1),meanRMSByFreq(:,3),'bo');
+set(yyh, 'box','off');
+xlabel('Frequency (Hz)')
+ylabel(yyh(1),'Steady-state RMS (pA)');
+ylabel(yyh(2),'Normalized steady-state RMS');
+
+%% Plot traces (FAT218) at 10, 100, 500 Hz
+% 2,4,6
+
+respTraces = sinePeaks{9,2}([2 4 6]);
+respTraces = cellfun(@(x) mean(x,2),respTraces,'un',0);
+respTraces = [respTraces{:}];
+respMean = mean(respTraces(16499:17499,:),1);
+
+stimTraces = sinePeaksPD{9,2}([2,4,6]);
+stimTraces = cellfun(@(x) mean(x,2),stimTraces,'un',0);
+stimTraces = [stimTraces{:}];
+tVec = ((1:length(stimTraces))/sf)';
+
+yyh = plotyy(tVec,respTraces,tVec,stimTraces);
+hline(respMean(1),'b'); hline(respMean(2),'r'); hline(respMean(3),'m');
+
+%% Plot zoomed traces for 5 cycles
+% Calculate time
+% 10Hz = 500ms
+% 100Hz = 50ms
+% 500Hz = 10ms
+cycleLims = [1.25 1.7500; 1.7000 1.7500; 1.7400 1.7500];
+
+yyh = cell(0);
+for i = 1:3
+    figure();
+    yyh{i} = plotyy(tVec,respTraces(:,i),tVec,stimTraces(:,i));
+    hline(respMean(i),'k:');
+    set(yyh{i},'XLim',cycleLims(i,:));
+    set(yyh{i}(2),'YLim',[-1 0]);
+    set(yyh{i}(1),'Ylim',[respMean(i)-10e-12 respMean(i)+10e-12]);
+end
+%% Use steps to plot resonance in stim
+a = cellfun(@(x) x{:}, sinePeaksPD(:,2),'un',0);
+b = [a{:}];
+c = mean(b,2);
+sf = 10000;
+onStim = c(1505:4500);
+offStim = c(4505:7500);
+
+plot(onStim);
+% [pxx,f] = pwelch(onStim,2000,[],[],sf);
+% plot(f,pxx)
+% set(gca,'XScale','log','YScale','log');
